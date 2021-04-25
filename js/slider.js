9d9bd6633bd4847ca38f354243586cb88d40058f
Poly <rodriguezpaula606@gmail.com>
2021-04-24 20:46:20 -0500

    educacion y experiencia

--- js/slider.js
+++ js/slider.js
@@ -1,89 +1,21 @@
-$('.slider').each(function() {
-    var $this = $(this);
-    var $group = $this.find('.slide_group');
-    var $slides = $this.find('.slide');
-    var bulletArray = [];
-    var currentIndex = 0;
-    var timeout;
-    
-    function move(newIndex) {
-      var animateLeft, slideLeft;
-      
-      advance();
-      
-      if ($group.is(':animated') || currentIndex === newIndex) {
-        return;
-      }
-      
-      bulletArray[currentIndex].removeClass('active');
-      bulletArray[newIndex].addClass('active');
-      
-      if (newIndex > currentIndex) {
-        slideLeft = '100%';
-        animateLeft = '-100%';
-      } else {
-        slideLeft = '-100%';
-        animateLeft = '100%';
-      }
-      
-      $slides.eq(newIndex).css({
-        display: 'block',
-        left: slideLeft
-      });
-      $group.animate({
-        left: animateLeft
-      }, function() {
-        $slides.eq(currentIndex).css({
-          display: 'none'
-        });
-        $slides.eq(newIndex).css({
-          left: 0
-        });
-        $group.css({
-          left: 0
-        });
-        currentIndex = newIndex;
-      });
-    }
-    
-    function advance() {
-      clearTimeout(timeout);
-      timeout = setTimeout(function() {
-        if (currentIndex < ($slides.length - 1)) {
-          move(currentIndex + 1);
-        } else {
-          move(0);
-        }
-      }, 4000);
-    }
-    
-    $('.next_btn').on('click', function() {
-      if (currentIndex < ($slides.length - 1)) {
-        move(currentIndex + 1);
-      } else {
-        move(0);
-      }
-    });
-    
-    $('.previous_btn').on('click', function() {
-      if (currentIndex !== 0) {
-        move(currentIndex - 1);
-      } else {
-        move(3);
-      }
-    });
-    
-    $.each($slides, function(index) {
-      var $button = $('<a class="slide_btn">&bull;</a>');
-      
-      if (index === currentIndex) {
-        $button.addClass('active');
-      }
-      $button.on('click', function() {
-        move(index);
-      }).appendTo('.slide_buttons');
-      bulletArray.push($button);
-    });
-    
-    advance();
-  });
+$('.slide-nav').on('click', function(e) {
+  e.preventDefault();
+  // get current slide
+  var current = $('.flex--active').data('slide'),
+    // get button data-slide
+    next = $(this).data('slide');
+
+  $('.slide-nav').removeClass('active');
+  $(this).addClass('active');
+
+  if (current === next) {
+    return false;
+  } else {
+    $('.slider__warpper').find('.flex__container[data-slide=' + next + ']').addClass('flex--preStart');
+    $('.flex--active').addClass('animate--end');
+    setTimeout(function() {
+      $('.flex--preStart').removeClass('animate--start flex--preStart').addClass('flex--active');
+      $('.animate--end').addClass('animate--start').removeClass('animate--end flex--active');
+    }, 800);
+  }
+});
